a5272b651d5ced9cc1c675a3acbcd0a6fcd3454c
fish-xiang <lix930@163.com>
2016-09-17 20:29:22 +0800

    在ubuntu下测试git

---
+++ test/src/Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class  Hello {
+	public static void main (String[] args) {
+		System.out.println("weishenme buneng shuru zhongwen!"); 
+	}
+
+}
